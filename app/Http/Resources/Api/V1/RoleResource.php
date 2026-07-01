e2e868f20f435f960ef1e3781793f18d42a627e4
Rubel Mahmud <vxlrubel@gmail.com>
2026-07-02 01:12:47 +0600

    feat(resource): add RoleResource

---
+++ app/Http/Resources/Api/V1/RoleResource.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources\Api\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RoleResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'school_id' => $this->school_id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'description' => $this->description,
+            'permissions' => PermissionResource::collection($this->whenLoaded('permissions')),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
