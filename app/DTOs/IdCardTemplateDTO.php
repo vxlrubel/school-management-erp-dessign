c8d5beded65bcb516f1fdd1c0ac1b1f37663f065
Rubel Mahmud <vxlrubel@gmail.com>
2026-07-02 01:10:03 +0600

    feat(dto): add IdCardTemplateDTO for ID card template data

---
+++ app/DTOs/IdCardTemplateDTO.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DTOs;
+
+class IdCardTemplateDTO
+{
+    public function __construct(
+        public readonly ?int $id = null,
+        public readonly ?int $school_id = null,
+        public readonly string $name = '',
+        public readonly string $html = '',
+    ) {}
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            id: $data['id'] ?? null,
+            school_id: $data['school_id'] ?? null,
+            name: $data['name'] ?? '',
+            html: $data['html'] ?? '',
+        );
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'school_id' => $this->school_id,
+            'name' => $this->name,
+            'html' => $this->html,
+        ];
+    }
+}
