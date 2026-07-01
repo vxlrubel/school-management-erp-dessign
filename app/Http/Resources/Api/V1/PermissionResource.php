fb2d8e6045891cb97ef0c28e5e549d4631f27ee2
Rubel Mahmud <vxlrubel@gmail.com>
2026-07-02 01:12:47 +0600

    feat(resource): add NotificationResource

---
+++ app/Http/Resources/Api/V1/PermissionResource.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources\Api\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PermissionResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'description' => $this->description,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
