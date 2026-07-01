25c4f965b3c7042b2066801fa888144243fe7a91
Rubel Mahmud <vxlrubel@gmail.com>
2026-07-02 01:12:46 +0600

    feat(resource): add LeaveRequestResource

---
+++ app/Http/Resources/Api/V1/LeaveRequestResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Api\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LeaveRequestResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'school_id' => $this->school_id,
+            'user_id' => $this->user_id,
+            'leave_type_id' => $this->leave_type_id,
+            'reason' => $this->reason,
+            'start_date' => $this->start_date,
+            'end_date' => $this->end_date,
+            'status' => $this->status,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
