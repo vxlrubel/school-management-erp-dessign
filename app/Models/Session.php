d99ef274bbe838f978b87b7596965d64517b94eb
Rubel Mahmud <vxlrubel@gmail.com>
2026-07-02 01:09:03 +0600

    feat(model): add Session model for academic year management

---
+++ app/Models/Session.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\HasSchool;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Session extends Model
+{
+    use HasSchool, SoftDeletes;
+
+    protected $fillable = [
+        'school_id',
+        'name',
+        'current',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'current' => 'boolean',
+        ];
+    }
+
+    public function studentAcademics(): HasMany
+    {
+        return $this->hasMany(StudentAcademic::class);
+    }
+}
