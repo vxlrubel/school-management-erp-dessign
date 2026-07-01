ec2a21d623e78e37cdd44b9110475797359f1d75
Rubel Mahmud <vxlrubel@gmail.com>
2026-07-02 01:12:45 +0600

    feat(resource): add FeeHeadResource

---
+++ app/Http/Resources/Api/V1/FeeHeadResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources\Api\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FeeHeadResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'school_id' => $this->school_id,
+            'name' => $this->name,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'fee_structures' => FeeStructureResource::collection($this->whenLoaded('feeStructures')),
+        ];
+    }
+}
