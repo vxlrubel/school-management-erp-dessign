741e9fc7b292cf99a1bfcfe7fd1ca6632ba5fc87
Rubel Mahmud <vxlrubel@gmail.com>
2026-07-02 01:12:47 +0600

    feat(resource): add PeriodResource

---
+++ app/Http/Resources/Api/V1/PeriodResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources\Api\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PeriodResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'school_id' => $this->school_id,
+            'name' => $this->name,
+            'start_time' => $this->start_time,
+            'end_time' => $this->end_time,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
