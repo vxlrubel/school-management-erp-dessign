ab3d8c2146f89da7bbc27578a772a6d5977a5e34
Rubel Mahmud <vxlrubel@gmail.com>
2026-07-02 01:12:46 +0600

    feat(resource): add MediaResource

---
+++ app/Http/Resources/Api/V1/MediaResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Api\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MediaResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'school_id' => $this->school_id,
+            'disk' => $this->disk,
+            'file_name' => $this->file_name,
+            'path' => $this->path,
+            'mime' => $this->mime,
+            'size' => $this->size,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
---
+++ app/Http/Resources/Api/V1/PageResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\Api\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PageResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'school_id' => $this->school_id,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'content' => $this->content,
+            'status' => $this->status,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
