b2dadb9e1f2ef5848da1426e9bc6dbed765a5094
Rubel Mahmud <vxlrubel@gmail.com>
2026-07-02 01:09:05 +0600

    feat(model): add Attendance model for daily attendance sessions

---
+++ app/Models/Attendance.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\HasSchool;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Attendance extends Model
+{
+    use HasSchool, SoftDeletes;
+
+    protected $fillable = [
+        'school_id',
+        'attendance_date',
+        'type',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'attendance_date' => 'date:Y-m-d',
+        ];
+    }
+
+    public function attendanceRecords(): HasMany
+    {
+        return $this->hasMany(AttendanceRecord::class);
+    }
+}
