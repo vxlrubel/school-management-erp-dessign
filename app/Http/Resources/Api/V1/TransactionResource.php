41fb00d157f9ba84a2d59992d537d1569cbd4beb
Rubel Mahmud <vxlrubel@gmail.com>
2026-07-02 01:12:50 +0600

    feat(resource): add TransactionResource

---
+++ app/Http/Resources/Api/V1/TransactionResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\Api\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TransactionResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'school_id' => $this->school_id,
+            'invoice' => $this->invoice,
+            'amount' => $this->amount,
+            'payment_method' => $this->payment_method,
+            'status' => $this->status,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
